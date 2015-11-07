0e922b0d205b93626d1e24914a3bfc333cf3b02b
Andrey Gornushkin <andrey.gornushkin@gmail.com>
2015-11-07 18:37:12 +0500

    Functional tests updated to check we can input a todo item

--- functional_tests.py
+++ functional_tests.py
@@ -1,5 +1,6 @@
 import unittest
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
 
 class NewVisitorTest(unittest.TestCase):
@@ -16,21 +17,29 @@ class NewVisitorTest(unittest.TestCase):
         # to check out its homepage
         # She notices the page title and header mention to-do lists
         assert 'Todo' in self.browser.title
+        assert 'Todo' in self.browser.find_element_by_tag_name('h1').text
 
-        self.fail('Finish the test')
         # She is invited to enter a to-do item straight away
+        input_box = self.browser.find_element_by_id('id_new_item')
+        assert input_box.get_attribute('placeholder') == 'Enter a todo item'
 
-        # She types "Buy peacock feathers" into a text box (Edith's hobby
-        # is tying fly-fishing lures)
+        # She types "Buy tickets to the moon" into a text box (Edith's hobby
+        # is travelling)
+        user_input_1 = "Buy tickets to the Moon"
+        input_box.send_keys(user_input_1)
+        input_box.send_keys(Keys.ENTER)
 
         # When she hits enter, the page updates, and now the page lists
-        # "1: Buy peacock feathers" as an item in a to-do list
-
+        # "1: Buy tickets to the Moon" as an item in a to-do list
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        assert any(row.text == user_input_1 for row in rows)
         # There is still a text box inviting her to add another item. She
-        # enters "Use peacock feathers to make a fly" (Edith is very methodical)
+        # enters "Go to rover driving lesson" (Edith is very methodical)
+        user_input_2 = "Go to rover driving lesson"
 
         # The page updates again, and now shows both items on her list
-
+        self.fail("Finish the test")
         # Edith wonders whether the site will remember her list. Then she sees
         # that the site has generated a unique URL for her -- there is some
         # explanatory text to that effect.
