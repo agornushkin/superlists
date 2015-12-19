2c9225ffd0f0e2914aa6ddb95a05c499ed99a6c0
Andrey Gornushkin <andrey.gornushkin@gmail.com>
2015-12-19 15:45:27 +0500

    Implement test_validation FT

--- functional_tests/test_validation.py
+++ functional_tests/test_validation.py
@@ -6,21 +6,23 @@ from .base import FunctionalTest
 class ValidationTest(FunctionalTest):
     def test_cannot_add_empty_list_items(self):
         # Edith go to home page
-
+        self.browser.get(self.server_url)
         # Tries to add an empty item
-
+        self.browser.find_element_by_id('id_new_item').send_keys('\n')
         # Gets an error
-
+        error = self.browser.find_element_by_css_selector('.error')
+        self.assertEqual(error.text, "You can't have an empty list item")
         # Corrects
-
+        self.browser.find_element_by_id('id_new_item').send_keys('Buy milk\n')
         # Item is added, no error
-
+        self.check_for_row_in_list_table('1: Buy milk')
         # Tries to add another empty item
-
+        self.browser.find_element_by_id('id_new_item').send_keys('\n')
         # Gets an error
-
+        error = self.browser.find_element_by_css_selector('.error')
+        self.assertEqual(error.text, "You can't have an empty list item")
         # Corrects
-
+        self.browser.find_element_by_id('id_new_item').send_keys('Make tea\n')
         # Both items are now on the list, no errors
-
-        self.fail('Write me!')
+        self.check_for_row_in_list_table('1: Buy milk')
+        self.check_for_row_in_list_table('2: Make tea')
