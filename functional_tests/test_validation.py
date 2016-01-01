f81cbcae0b29e7fb7f0557a74b806e9104589fe5
Andrey Gornushkin <andrey.gornushkin@gmail.com>
2016-01-02 02:35:40 +0500

    FT and model implementation for dupilcate item validation

--- functional_tests/test_validation.py
+++ functional_tests/test_validation.py
@@ -14,7 +14,7 @@ class ValidationTest(FunctionalTest):
         self.assertEqual(error.text, "You can't have an empty list item")
         # Corrects
         self.find_input_box_for_new_item().send_keys('Buy milk\n')
-        # Item is added, no errorus
+        # Item is added, no errors
         self.check_for_row_in_list_table('1: Buy milk')
         # Tries to add another empty item
         self.find_input_box_for_new_item().send_keys('\n')
@@ -26,3 +26,15 @@ class ValidationTest(FunctionalTest):
         # Both items are now on the list, no errors
         self.check_for_row_in_list_table('1: Buy milk')
         self.check_for_row_in_list_table('2: Make tea')
+
+    def test_cannot_add_duplicate_items(self):
+        # Edith go to home page
+        self.browser.get(self.server_url)
+        # Adds an item
+        self.find_input_box_for_new_item().send_keys('Buy an island\n')
+        self.check_for_row_in_list_table('1: Buy an island')
+
+        # Accidentally tries to add the same item again
+        self.find_input_box_for_new_item().send_keys('Buy an island\n')
+        error = self.browser.find_element_by_css_selector('.has-error')
+        self.assertEqual(error.text, "You've already got this in your list")
